5cf777899b56f4f54e5c08b904fcc43b26f471e5
winverse <public.winverse@gmail.com>
2025-09-17 14:54:06 +0900

    feat: update 06

---
+++ 06-search-parameters/src/data/category.js
@@ -0,0 +1,17 @@
+export const categories = [
+  {
+    id: 'electronics',
+    name: 'Electronics',
+    icon: '💻',
+  },
+  {
+    id: 'furniture',
+    name: 'Furniture',
+    icon: '🛋️',
+  },
+  {
+    id: 'clothing',
+    name: 'Clothing',
+    icon: '👕',
+  },
+];
